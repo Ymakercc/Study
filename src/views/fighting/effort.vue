6153e541cde2f7a64d6bac14779dacfff96a7ebc
Shimmer6 <136464677+Shimmer6@users.noreply.github.com>
2024-06-09 20:32:23 +0800

    Update effort.vue
    学习

--- src/views/fighting/effort.vue
+++ src/views/fighting/effort.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { reactive } from "vue";
-import { ElMessage } from "element-plus";
+import { reactive } from 'vue';
+import { ElMessage } from 'element-plus';
 const config = reactive({
-  max: 3
+  max: 3,
 });
 const open = () => {
-  ElMessage("ZTT.");
+  ElMessage('ZTT.');
 };
-import { computed, ref } from "vue";
-import zhCn from "element-plus/dist/locale/zh-cn.mjs";
-import en from "element-plus/dist/locale/en.mjs";
+import { computed, ref } from 'vue';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+import en from 'element-plus/dist/locale/en.mjs';
 
-const language = ref("zh-cn");
-const locale = computed(() => (language.value === "zh-cn" ? zhCn : en));
+const language = ref('zh-cn');
+const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en));
 
 const toggle = () => {
-  language.value = language.value === "zh-cn" ? "en" : "zh-cn";
+  language.value = language.value === 'zh-cn' ? 'en' : 'zh-cn';
 };
 defineOptions({
-  name: ""
+  name: '',
 });
 </script>
 
@@ -29,10 +29,18 @@ defineOptions({
 <style lang="scss" scoped></style>
 
 <template>
-  <div class="">
-    <span>每行24分栏布局</span>
-    <el-row :gutter="20">
-      <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+  <div class="effort">
+    <el-row :gutter="20" class="elrow" type="flex">
+      <el-col :span="24" :offset="0">
+        <el-card shadow="hover" :body-style="{ padding: '150px' }">
+          <template #header>
+            <div>
+              <span>每行24分栏布局</span>
+            </div>
+          </template>
+          <!-- card body -->
+        </el-card>
+
         <el-config-provider :message="config">
           <el-button @click="open">测试</el-button>
         </el-config-provider>
@@ -41,7 +49,7 @@ defineOptions({
   </div>
 
   <div>
-    <el-button mb-2 @click="toggle">Switch Language</el-button>
+    <el-button mb-2 @click="toggle">切换语言</el-button>
     <br />
 
     <el-config-provider :locale="locale">
