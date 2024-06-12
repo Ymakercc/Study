7ef8c8a608e871ae7e049a02c7c3b3baa09efb9b
Yunjiang0 <99641229+Yunjiang0@users.noreply.github.com>
2024-06-12 20:44:48 +0800

    合并提交

--- src/views/dataserach/components/curve.vue
+++ src/views/dataserach/components/curve.vue
@@ -35,15 +35,6 @@ const option = {
     padding: 0,
     formatter: (p: any) => {
       let dom = `<div style="width:100%; height: 70px !important; display:flex;flex-direction: column;justify-content: space-between;padding:10px;box-sizing: border-box;
-  tooltip: {
-    trigger: 'axis',
-    backgroundColor: 'transparent',
-    axisPointer: {
-      type: 'none',
-    },
-    padding: 0,
-    formatter: (p: any) => {
-      let dom = `<div style="width:100%; height: 70px !important; display:flex;flex-direction: column;justify-content: space-between;padding:10px;box-sizing: border-box;
       color:#fff; background: #6B9DFE;border-radius: 4px;font-size:14px; ">
         <div style="display: flex; align-items: center;"> <div style="width:5px;height:5px;background:#ffffff;border-radius: 50%;margin-right:5px"></div>平台 :  ${p[0].name}</div>
         <div style="display: flex;align-items: center;"><div style="width:5px;height:5px;background:#ffffff;border-radius: 50%;margin-right:5px"></div>数据量 :  ${p[0].value}</div>
