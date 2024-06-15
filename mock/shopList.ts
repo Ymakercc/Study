8d032abac46c36ff7a6a921ffdea8adb8c142f52
Yamiker <ymakercc@gmail.com>
2024-06-15 19:22:21 +0800

    新增发货页面

--- mock/shopList.ts
+++ mock/shopList.ts
@@ -12,41 +12,57 @@ export default defineFakeRoute([
             {
               "Id": 1,
               "username": "John Doe",
-              "email": "john.doe@example.com",
-              "mobile": "1234567890",
-              "role_name": "管理员",
+              "Ordernumber": "10086332100",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
               "mg_state": true
             },
             {
               "Id": 2,
               "username": "Jane Smith",
-              "email": "jane.smith@example.com",
-              "mobile": "0987654321",
-              "role_name": "用户",
+              "Ordernumber": "10086332101",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
               "mg_state": false
             },
             {
               "Id": 3,
               "username": "Alice Johnson",
-              "email": "alice.johnson@example.com",
-              "mobile": "1122334455",
-              "role_name": "用户",
+              "Ordernumber": "10086332102",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
               "mg_state": true
             },
             {
               "Id": 4,
               "username": "Bob Brown",
-              "email": "bob.brown@example.com",
-              "mobile": "5566778899",
-              "role_name": "管理员",
+              "Ordernumber": "10086332103",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
               "mg_state": false
             },
             {
               "Id": 5,
               "username": "Charlie Davis",
-              "email": "charlie.davis@example.com",
-              "mobile": "6677889900",
-              "role_name": "用户",
+              "Ordernumber": "10086332104",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
+              "mg_state": true
+            },
+            {
+              "Id": 6,
+              "username": "Charlie Davis",
+              "Ordernumber": "10086332104",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
+              "mg_state": true
+            },
+            {
+              "Id": 7,
+              "username": "Charlie Davis",
+              "Ordernumber": "10086332104",
+              "Timeofpurchase": "2019-03-14 16:42",
+              "Paymenttime": "2019-03-14 19:29",
               "mg_state": true
             }
           ]
--- src/views/fighting/effort.vue
+++ src/views/fighting/effort.vue
@@ -4,7 +4,8 @@ defineOptions({
 });
 import { ref, reactive, watch, computed } from 'vue';
 import { getUserList } from '@/api/shopList'; // 引入封装好的函数
-
+import searchIcon from '@/assets/svg/search.svg?component';
+import Card from './components/Card.vue';
 // const userlist = ref([]);
 // const Alltotal = ref(0);
 
@@ -39,16 +40,28 @@ const Alltotal = ref(0);
 
 getUserList().then(res => {
   Alltotal.value = res.data.users.length;
+
   userlist.value = res.data.users;
   state.total = Alltotal.value; // 更新分页的总数
+  console.log(Alltotal.value);
+  console.log(state.total);
 });
 
 const state = reactive({
   page: 1,
-  limit: 2,
+  limit: 5,
   total: Alltotal.value,
   pageSizes: [2, 5, 10],
 });
+
+const cards = ref([
+  { title: '总订单', body: '555555', footer: '已完成' },
+  { title: '已完成', body: '789012', footer: '待处理' },
+  { title: '已取消', body: '789012', footer: '待处理' },
+  { title: '待收款', body: '789012', footer: '待处理' },
+  { title: '待发货', body: '789012', footer: '待处理' },
+  { title: '待收货', body: '123456', footer: '处理中' },
+]);
 
 const tableData = computed(() => {
   return userlist.value.slice(
@@ -65,6 +78,17 @@ const handleSizeChange = e => {
   state.limit = e;
   state.page = 1; // 当修改每页显示条数时，重置到第一页
 };
+
+const handleDeliver = row => {
+  row.mg_state = true;
+};
+
+//方法生效了但是应为数据是mock模拟的，mock里面没有写响应删除的参数因此视图不会跟新
+const handleDelete = index => {
+  tableData.value.splice(index, 1);
+  alert('操作已经成功，没有写向后端发起删除的请求，数据一致性，视图不更新');
+  console.log('1');
+};
 </script>
 
 <template>
@@ -79,7 +103,7 @@ const handleSizeChange = e => {
         >
           <template #header>Yummy hamburger</template>
           <!-- card body -->
-          <div class="card">
+          <!-- <div class="card">
             <div class="icon">
               <svg
                 viewBox="0 0 24 24"
@@ -92,91 +116,14 @@ const handleSizeChange = e => {
                 ></path>
               </svg>
             </div>
-            <strong> Instagram </strong>
+            <strong> 全部订单 </strong>
             <div class="card__body">
               Get UI elements that help you stand out.
             </div>
             <span>Follow us</span>
-          </div>
-
-          <div class="card">
-            <div class="icon">
-              <svg
-                viewBox="0 0 24 24"
-                height="24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11.984 16.815c2.596 0 4.706-2.111 4.706-4.707 0-1.409-.623-2.674-1.606-3.538-.346-.303-.735-.556-1.158-.748-.593-.27-1.249-.421-1.941-.421s-1.349.151-1.941.421c-.424.194-.814.447-1.158.749-.985.864-1.608 2.129-1.608 3.538 0 2.595 2.112 4.706 4.706 4.706zm.016-8.184c1.921 0 3.479 1.557 3.479 3.478 0 1.921-1.558 3.479-3.479 3.479s-3.479-1.557-3.479-3.479c0-1.921 1.558-3.478 3.479-3.478zm5.223.369h6.777v10.278c0 2.608-2.114 4.722-4.722 4.722h-14.493c-2.608 0-4.785-2.114-4.785-4.722v-10.278h6.747c-.544.913-.872 1.969-.872 3.109 0 3.374 2.735 6.109 6.109 6.109s6.109-2.735 6.109-6.109c.001-1.14-.327-2.196-.87-3.109zm2.055-9h-12.278v5h-1v-5h-1v5h-1v-4.923c-.346.057-.682.143-1 .27v4.653h-1v-4.102c-1.202.857-2 2.246-2 3.824v3.278h7.473c1.167-1.282 2.798-2 4.511-2 1.722 0 3.351.725 4.511 2h7.505v-3.278c0-2.608-2.114-4.722-4.722-4.722zm2.722 5.265c0 .406-.333.735-.745.735h-2.511c-.411 0-.744-.329-.744-.735v-2.53c0-.406.333-.735.744-.735h2.511c.412 0 .745.329.745.735v2.53z"
-                ></path>
-              </svg>
-            </div>
-            <strong> Instagram </strong>
-            <div class="card__body">
-              Get UI elements that help you stand out.
-            </div>
-            <span>Follow us</span>
-          </div>
-
-          <div class="card">
-            <div class="icon">
-              <svg
-                viewBox="0 0 24 24"
-                height="24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11.984 16.815c2.596 0 4.706-2.111 4.706-4.707 0-1.409-.623-2.674-1.606-3.538-.346-.303-.735-.556-1.158-.748-.593-.27-1.249-.421-1.941-.421s-1.349.151-1.941.421c-.424.194-.814.447-1.158.749-.985.864-1.608 2.129-1.608 3.538 0 2.595 2.112 4.706 4.706 4.706zm.016-8.184c1.921 0 3.479 1.557 3.479 3.478 0 1.921-1.558 3.479-3.479 3.479s-3.479-1.557-3.479-3.479c0-1.921 1.558-3.478 3.479-3.478zm5.223.369h6.777v10.278c0 2.608-2.114 4.722-4.722 4.722h-14.493c-2.608 0-4.785-2.114-4.785-4.722v-10.278h6.747c-.544.913-.872 1.969-.872 3.109 0 3.374 2.735 6.109 6.109 6.109s6.109-2.735 6.109-6.109c.001-1.14-.327-2.196-.87-3.109zm2.055-9h-12.278v5h-1v-5h-1v5h-1v-4.923c-.346.057-.682.143-1 .27v4.653h-1v-4.102c-1.202.857-2 2.246-2 3.824v3.278h7.473c1.167-1.282 2.798-2 4.511-2 1.722 0 3.351.725 4.511 2h7.505v-3.278c0-2.608-2.114-4.722-4.722-4.722zm2.722 5.265c0 .406-.333.735-.745.735h-2.511c-.411 0-.744-.329-.744-.735v-2.53c0-.406.333-.735.744-.735h2.511c.412 0 .745.329.745.735v2.53z"
-                ></path>
-              </svg>
-            </div>
-            <strong> Instagram </strong>
-            <div class="card__body">
-              Get UI elements that help you stand out.
-            </div>
-            <span>Follow us</span>
-          </div>
-
-          <div class="card">
-            <div class="icon">
-              <svg
-                viewBox="0 0 24 24"
-                height="24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11.984 16.815c2.596 0 4.706-2.111 4.706-4.707 0-1.409-.623-2.674-1.606-3.538-.346-.303-.735-.556-1.158-.748-.593-.27-1.249-.421-1.941-.421s-1.349.151-1.941.421c-.424.194-.814.447-1.158.749-.985.864-1.608 2.129-1.608 3.538 0 2.595 2.112 4.706 4.706 4.706zm.016-8.184c1.921 0 3.479 1.557 3.479 3.478 0 1.921-1.558 3.479-3.479 3.479s-3.479-1.557-3.479-3.479c0-1.921 1.558-3.478 3.479-3.478zm5.223.369h6.777v10.278c0 2.608-2.114 4.722-4.722 4.722h-14.493c-2.608 0-4.785-2.114-4.785-4.722v-10.278h6.747c-.544.913-.872 1.969-.872 3.109 0 3.374 2.735 6.109 6.109 6.109s6.109-2.735 6.109-6.109c.001-1.14-.327-2.196-.87-3.109zm2.055-9h-12.278v5h-1v-5h-1v5h-1v-4.923c-.346.057-.682.143-1 .27v4.653h-1v-4.102c-1.202.857-2 2.246-2 3.824v3.278h7.473c1.167-1.282 2.798-2 4.511-2 1.722 0 3.351.725 4.511 2h7.505v-3.278c0-2.608-2.114-4.722-4.722-4.722zm2.722 5.265c0 .406-.333.735-.745.735h-2.511c-.411 0-.744-.329-.744-.735v-2.53c0-.406.333-.735.744-.735h2.511c.412 0 .745.329.745.735v2.53z"
-                ></path>
-              </svg>
-            </div>
-            <strong> Instagram </strong>
-            <div class="card__body">
-              Get UI elements that help you stand out.
-            </div>
-            <span>Follow us</span>
-          </div>
-
-          <div class="card">
-            <div class="icon">
-              <svg
-                viewBox="0 0 24 24"
-                height="24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11.984 16.815c2.596 0 4.706-2.111 4.706-4.707 0-1.409-.623-2.674-1.606-3.538-.346-.303-.735-.556-1.158-.748-.593-.27-1.249-.421-1.941-.421s-1.349.151-1.941.421c-.424.194-.814.447-1.158.749-.985.864-1.608 2.129-1.608 3.538 0 2.595 2.112 4.706 4.706 4.706zm.016-8.184c1.921 0 3.479 1.557 3.479 3.478 0 1.921-1.558 3.479-3.479 3.479s-3.479-1.557-3.479-3.479c0-1.921 1.558-3.478 3.479-3.478zm5.223.369h6.777v10.278c0 2.608-2.114 4.722-4.722 4.722h-14.493c-2.608 0-4.785-2.114-4.785-4.722v-10.278h6.747c-.544.913-.872 1.969-.872 3.109 0 3.374 2.735 6.109 6.109 6.109s6.109-2.735 6.109-6.109c.001-1.14-.327-2.196-.87-3.109zm2.055-9h-12.278v5h-1v-5h-1v5h-1v-4.923c-.346.057-.682.143-1 .27v4.653h-1v-4.102c-1.202.857-2 2.246-2 3.824v3.278h7.473c1.167-1.282 2.798-2 4.511-2 1.722 0 3.351.725 4.511 2h7.505v-3.278c0-2.608-2.114-4.722-4.722-4.722zm2.722 5.265c0 .406-.333.735-.745.735h-2.511c-.411 0-.744-.329-.744-.735v-2.53c0-.406.333-.735.744-.735h2.511c.412 0 .745.329.745.735v2.53z"
-                ></path>
-              </svg>
-            </div>
-            <strong> Instagram </strong>
-            <div class="card__body">
-              Get UI elements that help you stand out.
-            </div>
-            <span>Follow us</span>
+          </div> -->
+          <div v-for="(card, index) in cards" :key="index">
+            <Card :title="card.title" :body="card.body" :footer="card.footer" />
           </div>
         </el-card>
       </el-col>
@@ -186,11 +133,11 @@ const handleSizeChange = e => {
       <el-col>
         <el-card>
           <template #header>订单列表</template>
-          <el-row :gutter="20">
+          <el-row :gutter="20" style="padding-bottom: 10px">
             <el-col :span="8">
               <el-input placeholder="请输入内容">
                 <template #append>
-                  <el-icon><search /></el-icon>
+                  <searchIcon></searchIcon>
                 </template>
               </el-input>
             </el-col>
@@ -202,23 +149,49 @@ const handleSizeChange = e => {
           <el-table :data="tableData" border stripe>
             <el-table-column type="index"></el-table-column>
             <el-table-column label="姓名" prop="username"></el-table-column>
-            <el-table-column label="邮箱" prop="email"></el-table-column>
-            <el-table-column label="电话" prop="mobile"></el-table-column>
-            <el-table-column label="角色" prop="role_name"></el-table-column>
+            <el-table-column
+              label="订单号"
+              prop="Ordernumber"
+            ></el-table-column>
+            <el-table-column
+              label="购买时间"
+              prop="Timeofpurchase"
+            ></el-table-column>
+            <el-table-column
+              label="付款时间"
+              prop="Paymenttime"
+            ></el-table-column>
             <el-table-column label="状态" prop="mg_state">
               <template v-slot="scope">
                 <el-switch v-model="scope.row.mg_state" />
               </template>
             </el-table-column>
-            <el-table-column label="操作" width="180px"> </el-table-column>
+            <el-table-column label="操作" width="270px">
+              <template v-slot="scope">
+                <el-button
+                  type="primary"
+                  size="default"
+                  @click="handleDeliver(scope.row)"
+                  >发货</el-button
+                >
+                <el-button type="primary" size="default">详情</el-button>
+                <el-button
+                  type="primary"
+                  size="default"
+                  @click="handleDelete(scope.$index)"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
           </el-table>
           <!-- 页面区域 -->
           <el-pagination
-            layout="prev, pager, next, sizes"
+            layout="total,prev, pager, next, sizes"
             :total="state.total"
             :page-size="state.limit"
             :current-page="state.page"
             :page-sizes="state.pageSizes"
+            style="padding-top: 10px"
             @current-change="handleCurrentChange"
             @size-change="handleSizeChange"
           ></el-pagination>
